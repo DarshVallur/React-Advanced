9075f40f02fae68a8824646f12b6b815437a4475
DarshVallur <darshvallur@gmail.com>
2022-08-26 15:58:09 +0530

    Lazy Loading

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,64 @@
 import React from 'react';
 
 function About(props) {
-  return <h1>About</h1>;
+  return (
+    <h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga ipsam unde
+      deserunt, nulla dolores laborum perspiciatis quidem. Ipsum delectus
+      facere, illum ad aut repellat illo quasi animi molestiae, nisi
+      perspiciatis eveniet consectetur rem. Rem repellendus recusandae alias
+      quasi cumque similique dolorem repellat corrupti debitis magni. Atque
+      deserunt adipisci asperiores laborum animi veniam in, rem earum distinctio
+      esse ullam neque laudantium, error, porro harum blanditiis eligendi autem
+      minus necessitatibus saepe incidunt magni maiores quas officia! Ducimus,
+      temporibus magnam natus deleniti accusamus nulla autem, maiores dolore
+      enim omnis iste doloribus possimus quisquam perspiciatis repudiandae nam
+      hic deserunt reiciendis dicta qui id mollitia nobis? Atque voluptatem,
+      exercitationem ratione nam accusamus explicabo nisi officiis mollitia
+      asperiores nulla dolores, reprehenderit quis cumque autem ab, est corporis
+      non hic assumenda minima porro iusto fuga voluptas! Impedit perspiciatis
+      natus officia atque dolorem, iusto saepe consequatur unde ipsa sit
+      repellat, quae blanditiis rem obcaecati dignissimos, accusantium
+      voluptatem tempora nostrum! Dolorem consequatur quidem, alias, odio,
+      commodi nesciunt officia velit voluptatibus inventore blanditiis facere
+      illum rerum amet mollitia fugiat laborum asperiores sed ab necessitatibus
+      et animi accusantium! Quasi nisi ducimus accusantium exercitationem vel
+      facilis numquam sint ratione maiores voluptatem perspiciatis porro fuga
+      recusandae deleniti eveniet ipsa, distinctio unde doloribus sunt
+      voluptate, a placeat! In delectus cupiditate corrupti minima architecto
+      earum id, quos consequatur dolores perspiciatis expedita rem nulla ipsum
+      sapiente fuga asperiores, quam beatae, reiciendis ipsa qui iusto nesciunt
+      amet similique? Perferendis laborum perspiciatis temporibus excepturi
+      eaque debitis! Quidem est totam nesciunt tenetur possimus quibusdam
+      deleniti molestias itaque quis iste tempora dignissimos dolores sequi
+      cupiditate maiores, neque sunt laboriosam dolore rem? Sapiente, autem
+      molestiae? Iste eius porro impedit a, asperiores assumenda rem, laboriosam
+      quas maiores magni eum mollitia deserunt. Quidem perferendis expedita,
+      architecto illo impedit enim voluptatum eveniet optio explicabo molestias
+      provident placeat magni voluptas delectus voluptates asperiores! Aperiam,
+      asperiores amet aspernatur enim nam obcaecati mollitia perspiciatis!
+      Ducimus deleniti doloremque saepe fugit reprehenderit velit, cumque quo
+      modi aliquam, debitis expedita, nam impedit earum dolore molestias magni
+      sequi at! Maiores dignissimos repellendus quaerat ex mollitia quidem nam
+      nobis? Eaque repudiandae alias hic explicabo animi dolorem sed, dolores
+      maiores aliquid unde a, repellendus mollitia, ut doloremque voluptate.
+      Sint eligendi fugit maiores eum, laborum cumque at nam vel? Adipisci
+      maiores expedita dicta odio eius blanditiis quisquam ut? Hic explicabo
+      libero sit? Ducimus, cumque ab. Dicta vero aperiam hic illo earum. Quo non
+      a sapiente, maiores deleniti repellendus nisi labore sed dolor veritatis
+      vitae beatae aut voluptates. Ullam laboriosam aut, minima sapiente dicta
+      ipsum explicabo reprehenderit eveniet id quis sint unde cumque quae
+      recusandae doloremque, accusantium vel officiis! Dolorum itaque nostrum
+      quas aut magnam sequi sapiente necessitatibus atque repudiandae iste
+      impedit maxime odit laboriosam neque suscipit, illo adipisci reprehenderit
+      magni officia. Corrupti tempore error quae. Nulla impedit omnis earum?
+      Perspiciatis quidem facilis sunt beatae odio, eveniet nulla neque quis
+      consequatur fugiat laudantium non nemo quas aut enim illo culpa. Incidunt
+      nihil corrupti illum tempore saepe nesciunt aut iure, eius odit sapiente
+      tenetur maxime recusandae ad, sed eligendi dolorum excepturi. Ipsum ex
+      neque consequuntur animi!
+    </h1>
+  );
 }
 
 export default About;

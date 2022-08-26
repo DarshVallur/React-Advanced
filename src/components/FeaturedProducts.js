f5ae7625cb144679bf947affd260afb5c1923f24
DarshVallur <darshvallur@gmail.com>
2022-08-26 12:25:31 +0530

    Nested Router

---
+++ src/components/FeaturedProducts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FeaturedProducts(props) {
+  return <div>List of Featured Products</div>;
+}
+
+export default FeaturedProducts;
---
+++ src/components/NewProducts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewProdcuts(props) {
+  return <div>List of New Prodcuts</div>;
+}
+
+export default NewProdcuts;
